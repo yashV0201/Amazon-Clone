f5ab3b84352642c0431e81713924f4ab23ef7ac8
yashV0201 <vardhan.yash0201@gmail.com>
2024-04-09 21:52:13 +0530

    In progress

---
+++ data/cart.js
@@ -0,0 +1,55 @@
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+    cart=[{
+        productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity:1
+    },{
+        productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity:2
+    }];
+};
+
+
+function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
+
+
+
+export function addtoCart(productId){
+    let matchingItem;
+
+    cart.forEach((cartItem)=>{
+        if(productId === cartItem.productId){
+            matchingItem = cartItem;
+        }
+    });
+
+    if(matchingItem){
+        matchingItem.quantity +=1;
+    } else{
+        cart.push({
+            productId:productId,
+            quantity:1
+        });
+        
+    }
+
+    saveToStorage();
+
+} 
+
+export function removeFromCart(productId){
+    const newArr=[];
+
+    cart.forEach((cartItem)=>{
+        if(cartItem.productId !== productId){
+            newArr.push(cartItem);
+        }
+    })
+
+    cart = newArr;
+
+    saveToStorage();
+}
